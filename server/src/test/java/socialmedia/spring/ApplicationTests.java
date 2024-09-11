840cb36719365984d998a97bf47fbc7467979250
Aryan-RG <aryan.daksh@rategain.com>
2024-09-11 13:25:41 +0530

    Project Addition

---
+++ server/src/test/java/socialmedia/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package socialmedia.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
